f836b962ee206c046965075ecc7fd25b5a8a4884
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2024-10-21 23:02:17 -0400

    Update aecolor.py
    wtf

---
+++ aecolor/src/aecolor.py
@@ -0,0 +1,29 @@
+#### AECOLOR/SRC/AECOLOR 
+
+
+## IMPORTS ==== 
+
+
+## .. 
+
+
+## IMPORTS ==== 
+
+
+class AEColor(): 
+  def __init__(self) -> None: 
+    ## .. 
+    return None; 
+
+
+class Front(AEColor): 
+  def __init__(self) -> None: 
+    ## .. 
+  return None; 
+
+
+class Backdrop(AEColor): 
+  def __init__(self) -> None: 
+    ## .. 
+    return None; 
+
